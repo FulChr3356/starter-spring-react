99426839728459cae732c1e7382425754edc44b3
Christopher Fulton <51063994+FulChr3356@users.noreply.github.com>
2022-06-10 22:27:34 -0400

    Added spring
    Added spring files

---
+++ src/main/java/io/fulchr3356/starterspringreact/StarterSpringReactApplication.java
@@ -0,0 +1,13 @@
+package io.fulchr3356.starterspringreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterSpringReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterSpringReactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/fulchr3356/starterspringreact/StarterSpringReactApplicationTests.java
@@ -0,0 +1,13 @@
+package io.fulchr3356.starterspringreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterSpringReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
