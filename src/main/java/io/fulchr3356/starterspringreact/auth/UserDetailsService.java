eaadf54b43e94814a778f60bc7636b84ca221be6
Christopher Fulton <51063994+FulChr3356@users.noreply.github.com>
2022-06-15 01:59:51 -0400

    Added java auth classes

---
+++ src/main/java/io/fulchr3356/starterspringreact/auth/UserDetailsService.java
@@ -0,0 +1,8 @@
+package io.fulchr3356.starterspringreact.auth;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public interface UserDetailsService {
+    UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
+}
