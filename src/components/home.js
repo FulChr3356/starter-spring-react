2bb83b50f085c018fb17d25fef98718809e3a190
Christopher Fulton <51063994+FulChr3356@users.noreply.github.com>
2022-06-28 19:12:43 -0400

    added home

--- src/components/home.js
+++ src/components/home.js
@@ -1,24 +1,25 @@
 import React, { Component }  from 'react';
 import PrimarySearchAppBar from './PrimarySearchAppBar';
 import UserProfile from './userProfile';
-
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    Link
+  } from "react-router-dom";
 
 
 function Home(){
-
-
-
-
-
-
-
-
-
-
     return(
         <div>
             <PrimarySearchAppBar/>
-            <UserProfile/>
+            <BrowserRouter>
+            <Routes>
+            <Route path =  "/profile`" element =  {<UserProfile/>}/>
+            </Routes>
+            
+            </BrowserRouter>
+            
         </div>
     )
 }
